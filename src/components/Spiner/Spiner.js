f1e5916082fb7e6f2747ede5b49f0bf18c22ae3b
Ivan Vostrikov <ivanvostrikov@MacBook-Air-Ivan.local>
2020-05-23 12:55:47 +0700

    loading spinner

---
+++ src/components/Spiner/Spiner.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import './Spiner.css';
+
+const Spiner = () => {
+  return (
+    <div className="loadingio-spinner-double-ring-z6wgpebnmpm">
+      <div className="ldio-auvqzvu02pt">
+        <div></div>
+        <div></div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Spiner;
