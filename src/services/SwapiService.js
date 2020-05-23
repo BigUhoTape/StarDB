b75b631714c78b681fc9d1ed46f15e4cfa4b3883
Ivan Vostrikov <ivanvostrikov@MacBook-Air-Ivan.local>
2020-05-23 19:01:34 +0700

    get Person by Id by click on item in ItemList using componentDidUpdate

--- src/services/SwapiService.js
+++ src/services/SwapiService.js
@@ -79,8 +79,8 @@ export default class SwapiService {
       id: this._getByID(person),
       name: person.name,
       gender: person.gender,
-      birthYear: person.birthYear,
-      eyeColor: person.eyeColor
+      birthYear: person.birth_year,
+      eyeColor: person.eye_color
     }
   }
 }
